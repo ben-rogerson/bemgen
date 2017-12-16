38d27032c9489aca4f5d31788a7ec1758afce351
Ben Rogerson <info@benrogerson.com.au>
2017-12-16 15:25:02 +1030

    add what’s bem link

--- src/modules/Logo.js
+++ src/modules/Logo.js
@@ -1,6 +1,16 @@
 import React from 'react'
 
 const Logo = () =>
-  <div className="logo"><img className="logo__img" src="logo.svg" alt="Bem Gen" /></div>
+  <div className="header">
+    <div className="logo">
+      <img className="logo__img" src="logo.svg" alt="Bem Gen" />
+    </div>
+    <a
+    href="https://csswizardry.com/2013/01/mindbemding-getting-your-head-round-bem-syntax/"
+    className="whatbem"
+    target="_blank"
+    rel="noopener noreferrer"
+    >what&rsquo;s bem?</a>
+  </div>
 
 export default Logo
