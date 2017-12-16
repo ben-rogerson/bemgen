24454d17cda80594a33741c18df28979c4d2ebdf
Ben Rogerson <info@benrogerson.com.au>
2017-12-16 14:59:40 +1030

    better export label

--- src/modules/ExportButton.js
+++ src/modules/ExportButton.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ExportButton = ({toggleExport, isExport}) =>
   <button onClick={toggleExport} className="export-btn">
-    <span className="export-btn__inner"> {isExport ? 'back' : 'view export' }</span>
+    <span className="export-btn__inner">{ isExport ? 'back' : 'view code' }</span>
   </button>
 
 export default ExportButton
